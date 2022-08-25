c414f4b89e58183500217d4b85947692bdd65ea3
Deus <kirill2689@gmail.com>
2022-08-25 15:49:14 +0300

    registration fixes

--- todolist/settings.py
+++ todolist/settings.py
@@ -185,6 +185,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "core",
     "social_django",
+    "goals",
 ]
 
 AUTH_USER_MODEL = "core.User"
@@ -217,6 +218,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
