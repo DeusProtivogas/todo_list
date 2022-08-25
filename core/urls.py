9f0674117a619a89a7e5068026cd794546ba348d
Deus <kirill2689@gmail.com>
2022-08-25 10:48:47 +0300

    added registration

--- core/urls.py
+++ core/urls.py
@@ -18,3 +18,13 @@ Including another URLconf
 # from django.conf import settings
 # from django.conf.urls.static import static
 
+from django.urls import path
+
+from core import views
+
+urlpatterns = [
+    path("signup", views.SignupView.as_view()),
+    path("login", views.LoginView.as_view()),
+    path("profile", views.ProfileView.as_view()),
+    path("update_password", views.UpdatePasswordView.as_view()),
+]
